6d4b70b62d0b844877aa817c6686c87cce5e1e76
carrotderek <derekszeto@gmail.com>
2012-12-06 16:19:38 -0700

    Redid layout, added search, added action listeners to markers

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -4,60 +4,85 @@
 	"use strict";
 
 	$(document).ready(function () {
+		var map, markers;
+		var gettyImages = [];
+		initialize();
 
-		$("#tweets").liveTwitter('food', {rpp: 300000, filter: function(tweet){
+		$('#search').click(function(event) {
+			redrawMap();
+			searchAndPlot($('#searchBar').val());
+		});
+
+		function initialize() {
+
+			var southWest = new L.LatLng(-58.077876, -168.750000),
+			northEast = new L.LatLng(74.683250, 178.945313),
+			bounds = new L.LatLngBounds(southWest, northEast);
+
+			map = new L.Map('map', {
+				center: new L.LatLng(28.304381, -21.445313),
+				zoom: 1,
+				maxBounds: bounds
+			});
+
+			var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/30d1976aef28416ba4e9ed7cdd909ad8/50838/256/{z}/{x}/{y}.png',
+			cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18});
+			map.addLayer(cloudmade);
+
+			map.setView(new L.LatLng(40, -90), 3);                
+			markers = new L.MarkerClusterGroup();
+		}
+
+	function redrawMap() {
+		map.removeLayer(markers);
+		markers = new L.MarkerClusterGroup();
+	}
+
+	function searchAndPlot(searchTerm) {
+		fetchGettyImages();
+		$("#tweets").liveTwitter(searchTerm, {rpp: 300000, filter: function(tweet){
 			if(tweet.geo != null) {
-				console.log(tweet);
 				plotTweet(tweet);
 			}
 			return (tweet.geo != null);
 		}});
-
-		var gettyImages = [];
-		var southWest = new L.LatLng(-58.077876, -168.750000),
-		northEast = new L.LatLng(74.683250, 178.945313),
-		bounds = new L.LatLngBounds(southWest, northEast);
-
-		var map = new L.Map('map', {
-			center: new L.LatLng(28.304381, -21.445313),
-			zoom: 1,
-			maxBounds: bounds
-		});
-
-		var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/30d1976aef28416ba4e9ed7cdd909ad8/50838/256/{z}/{x}/{y}.png',
-		cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18});
-		map.addLayer(cloudmade);
-
-		map.setView(new L.LatLng(40, -90), 3);                
-
-		$.ajax({
-			type: 'GET',
-			url: 'test.json',
-			dataType: 'json',
-			success: parseGettyResponse,
-			error:  function(req, message) {
-				console.log('Error loading map: ' + message);
-			}
-		});
-		var markers = new L.MarkerClusterGroup();
-
-		function parseGettyResponse(response) {
-		$.each(response, function(i, item){
-			gettyImages.push(item.image);
-		});
+	}
+	
+	function fetchGettyImages() {
+			$.ajax({
+				type: 'GET',
+				url: 'test.json',
+				dataType: 'json',
+				success: function(response) {
+					$.each(response, function(i, item){
+						gettyImages.push(item);
+					});
+				},
+				error:  function(req, message) {
+					console.log('Error loading map: ' + message);
+				}
+			});
 	}
 
 	function plotTweet(tweet) {
 		var location = new L.LatLng(tweet.geo.coordinates[0], tweet.geo.coordinates[1]),
 			marker = new L.Marker(location),
-			num = Math.floor(Math.random() * (gettyImages.length + 1)),
-			msg = '<h3>Hello there</h3><img src="' + gettyImages[num] + '"/><p>' + tweet.text + '</p>';
+			num = Math.floor(Math.random() * (gettyImages.length + 1));
 
-		marker.bindPopup(msg);
+      var imageMarkup = '<div id="image-set"></div>';
+			for(var i = 0; i < 6; i++) {
+				imageMarkup += '<img src="' + gettyImages[num+i].image + '"/>';
+			}
+
+			console.log(imageMarkup);
+
+			marker.on('click', function() {
+				$('#info').replaceWith(imageMarkup);
+				// $('#image-set').after('<div id="tweet">' + tweet.text + '</div>');
+			});
+
 		markers.addLayer(marker);
 		map.addLayer(markers);
 	}
-
-
 });
 })(jQuery);
