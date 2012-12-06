9bb5322b7d4ead08511ad54e0189aceef2fda654
carrotderek <derekszeto@gmail.com>
2012-12-06 13:04:34 -0700

    Fixed plotting issue, changed map theme

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -5,45 +5,43 @@
 
 	$(document).ready(function () {
 
-		$('#map-container').hide();
-		$('#search').click(function() {
-			$('#map-container').slideDown('slow', function() {
-				initialize();
-			});
-		})
+		$("#tweets").liveTwitter('food', {rpp: 300000, filter: function(tweet){
+			if(tweet.geo != null) {
+				console.log(tweet);
+				plotTweet(tweet);
+			}
+			return (tweet.geo != null);
+		}});
 
-		var gettyImages = [],
-			map;
-		function initialize() {
-			var southWest = new L.LatLng(-58.077876, -168.750000),
-			northEast = new L.LatLng(74.683250, 178.945313),
-			bounds = new L.LatLngBounds(southWest, northEast);
+		var gettyImages = [];
+		var southWest = new L.LatLng(-58.077876, -168.750000),
+		northEast = new L.LatLng(74.683250, 178.945313),
+		bounds = new L.LatLngBounds(southWest, northEast);
 
-			map = new L.Map('map', {
-				center: new L.LatLng(28.304381, -21.445313),
-				zoom: 1,
-				maxBounds: bounds
-			});
+		var map = new L.Map('map', {
+			center: new L.LatLng(28.304381, -21.445313),
+			zoom: 1,
+			maxBounds: bounds
+		});
 
-			var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/30d1976aef28416ba4e9ed7cdd909ad8/5870/256/{z}/{x}/{y}.png',
-			cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18});
-			map.addLayer(cloudmade);
+		var cloudmadeUrl = 'http://{s}.tile.cloudmade.com/30d1976aef28416ba4e9ed7cdd909ad8/50838/256/{z}/{x}/{y}.png',
+		cloudmade = new L.TileLayer(cloudmadeUrl, {maxZoom: 18});
+		map.addLayer(cloudmade);
 
-			map.setView(new L.LatLng(40, -90), 3);                
+		map.setView(new L.LatLng(40, -90), 3);                
 
-			$.ajax({
-				type: 'GET',
-				url: 'test.json',
-				dataType: 'json',
-				success: parseGettyResponse,
-				error:  function(req, message) {
-					console.log('Error loading map: ' + message);
-				}
-			});
-			var markers = new L.MarkerClusterGroup();
-		}
+		$.ajax({
+			type: 'GET',
+			url: 'test.json',
+			dataType: 'json',
+			success: parseGettyResponse,
+			error:  function(req, message) {
+				console.log('Error loading map: ' + message);
+			}
+		});
+		var markers = new L.MarkerClusterGroup();
 
-	function parseGettyResponse(response) {
+		function parseGettyResponse(response) {
 		$.each(response, function(i, item){
 			gettyImages.push(item.image);
 		});
@@ -60,11 +58,6 @@
 		map.addLayer(markers);
 	}
 
-	$("#tweets").liveTwitter('food', {rpp: 300000, filter: function(tweet){
-		if(tweet.geo != null) {
-			console.log(tweet);
-			plotTweet(tweet);
-		}
-	}});
+
 });
 })(jQuery);
